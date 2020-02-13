81bd12e2f1e50a0fbe8c57b9997f73db2cf16583
louzhedong <louzhedong@xiaomai5.com>
2020-02-13 21:20:05 +0800

    添加TreeNode

---
+++ 树/TreeNode.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Michael 
+ * @Date: 2020-02-13 21:10:41 
+ * @Last Modified by: Michael
+ * @Last Modified time: 2020-02-13 21:11:09
+ * 二叉树节点
+ */
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
